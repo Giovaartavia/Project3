86a3d2dcbe9677a560fd0025088de44138c1d4d0
Giovaartavia <giovaartavia@hotmail.com>
2017-10-24 14:27:58 -0500

    Attempted to add GamePlayingScreen storyboard
    Attempted to add GamePlayingScreen storyboard

---
+++ Project 3/GamePlayingScreen.swift
@@ -0,0 +1,20 @@
+//
+//  GamePlayingScreen.swift
+//  Project 3
+//
+//  Created by Giovanni on 10/24/17.
+//  Copyright © 2017 Memory Leeks. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ViewGamePlayingScreen: UIViewController {
+    @IBOutlet weak var fightning: UIImageView!
+    @IBOutlet weak var testButton: UIButton!
+    
+   /* @IBAction func testOnClick(_ sender: Any) {
+        self.fightning.isHidden = true
+    }*/
+    
+}
