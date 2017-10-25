6c813bbf8ad8813659dda8d64a2b4e31d6dbf5dc
Giovaartavia <giovaartavia@hotmail.com>
2017-10-25 12:49:54 -0500

    Attempted to add classes and functions to GamePlayingScreen.swift
    Attempted to add classes and functions to GamePlayingScreen.swift

--- Project 3/GamePlayingScreen.swift
+++ Project 3/GamePlayingScreen.swift
@@ -11,9 +11,72 @@ import UIKit
 
 class ViewPlayGame: UIViewController {
     @IBOutlet weak var metal: UIImageView!
-    @IBAction func buttonIsPressed(_ sender: Any)
+    /*
+    class Mage
     {
-        self.metal.isHidden = true
+        let mageDeck = ["electric", "fire"]
+        var currDeck: [String]?
+        var currStamina = 2
+        var totalStamina: Int?
+        var health = 20
+        var attack = 0
+        var buffArr: [String]?
+        var debuff: String?
+        var shuffleCount = 2
     }
+    
+    class Warrior
+    {
+        let warriorDeck = ["electric", "fire"]
+        var currDeck: [String]?
+        var currStamina = 2
+        var totalStamina: Int?
+        var health = 20
+        var attack = 0
+        var buffArr: [String]?
+        var debuff: String?
+        var shuffleCount = 2
+    }
+    
+    //Used for testing
+    let player1 = Mage()
+    let player2 = Warrior()
+    //1 for player 1, 2 for player 2
+    var playerTurn = 0
+
+    func shuffleDeck(deck: [String])
+    {
+        //Shuffle both currDecks
+    }
+    
+    @IBOutlet weak var playCardButton: UIButton!
+    @IBOutlet weak var placeBottomButton: UIButton!
+    @IBOutlet weak var endTurnButton: UIButton!
+    @IBOutlet weak var shuffleButton: UIButton!
+    @IBOutlet weak var surrenderButton: UIButton!
+    
+    //Checks which cards was played and executes card effect.
+    @IBAction func playCardPress(_ sender: Any) {
+    }
+    
+    //Places first element in currDeck array to the last position.
+    @IBAction func placeBottomPress(_ sender: Any) {
+        
+    }
+    
+    //Passes turn to next player
+    @IBAction func endTurnPress(_ sender: Any) {
+    }
+    
+    //Randomizes currDeck
+    @IBAction func shufflePress(_ sender: Any) {
+    }
+    
+    //Ends game and takes user to replay storyboard
+    @IBAction func surrenderPress(_ sender: Any) {
+    }
+    
+    
+ */
 }
 
