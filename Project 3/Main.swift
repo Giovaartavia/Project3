285c9b9fcd1187aa0e0f87d3c21e4b4a9d541655
Dylan Herrig <dylan.herrig@gmail.com>
2017-11-01 23:17:32 -0500

    Completed Tutorial

--- Project 3/Main.swift
+++ Project 3/Main.swift
@@ -11,14 +11,36 @@ import UIKit
 
 class Tutorial: UIViewController {
     
+    @IBOutlet weak var TutorialImage: UIImageView!
     let tutorialArray = ["Tutorial1","Tutorial2","Tutorial3","Tutorial4","Tutorial5","Tutorial6","Tutorial7"];
+    var loc = 0;
+    
+    
     
     @IBAction func PrevScreen(_ sender: Any) {
-        
+        //print("PREV")
+        if(loc == 0)
+        {
+            loc = 6;
+        }
+        else
+        {
+            loc = loc-1;
+        }
+        TutorialImage.image = UIImage(named: tutorialArray[loc]);
     }
     
     @IBAction func NextScreen(_ sender: Any) {
-        
+        //print("NEXT")
+        if(loc == 6)
+        {
+            loc = 0;
+        }
+        else
+        {
+            loc = loc+1;
+        }
+        TutorialImage.image = UIImage(named: tutorialArray[loc]);
     }
     
 }
