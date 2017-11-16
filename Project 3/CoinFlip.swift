f5b7e78eac8f91cf5750c1b1997bb1db8d9f2704
James Glass <james.glass14@gmail.com>
2017-11-16 16:45:10 -0600

    Added more swift files to hold certain classes

---
+++ Project 3/CoinFlip.swift
@@ -0,0 +1,50 @@
+//
+//  CoinFlip.swift
+//  Project 3
+//
+//  Created by James Glass on 11/16/17.
+//  Copyright © 2017 Memory Leeks. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+///Class that contains the coin flip screen.
+class CoinFlip: UIViewController {
+    
+    ///Image used for the coin. Either Heads or Tails
+    @IBOutlet weak var coinImage: UIImageView!
+    ///Label used for indicating which player starts based of the coin flip
+    @IBOutlet weak var playerStartText: UILabel!
+    ///Button that hides the flip coin button
+    @IBOutlet weak var flipVisibility: UIButton!
+    ///Button that shows the next button
+    @IBOutlet weak var nextVisibility: UIButton!
+    //nextVisibility.isHidden = true;
+    
+    /// Function that flips a coin
+    ///
+    /// - Parameter sender: Player pressing button
+    @IBAction func flipCoin(_ sender: Any) {
+        let coinFlip = Int(arc4random_uniform(2))
+        //print ("Coin Result: \(coinFlip)")
+        
+        if(coinFlip == 1)
+        {
+            coinImage.image = UIImage(named: "Heads");
+            playerStartText.text="Player 1 Starts!";
+            flipVisibility.isHidden = true;
+            nextVisibility.isHidden = false;
+            playerStart = 1;
+        }
+        else
+        {
+            coinImage.image = UIImage(named: "Tails");
+            playerStartText.text="Player 2 Starts!";
+            flipVisibility.isHidden = true;
+            nextVisibility.isHidden = false;
+            playerStart = 2;
+        }
+        
+    }
+}
---
+++ Project 3/SelectDeck.swift
@@ -0,0 +1,88 @@
+//
+//  SelectDeck.swift
+//  Project 3
+//
+//  Created by James Glass on 11/16/17.
+//  Copyright © 2017 Memory Leeks. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+/// Class that handles player 1's class/deck selection
+class SelectionDeck1: UIViewController {
+    var currDeck = ["Initial"];
+    override func viewDidLoad() {
+        //Nothing yet :)
+    }
+    @IBOutlet weak var segmentSelect: UISegmentedControl!
+    
+    let warriorDeck = ["Throwing-Knife-Deck", "Throwing-Knife-Deck","Liquid-Courage-Deck","Liquid-Courage-Deck","Liquid-Courage-Deck","Brass-Knuckles-Deck", "Brass-Knuckles-Deck", "Disarm-Deck", "Disarm-Deck", "Blacksmith-Deck", "Smoke-Bomb-Deck", "Smoke-Bomb-Deck", "Double-Edge-Deck", "Health-Potion-Deck", "Health-Potion-Deck", "Bad-Medicine-Deck", "Bad-Medicine-Deck", "Sword-Strike-Deck", "Sword-Strike-Deck", "Sword-Strike-Deck"]
+    
+    let mageDeck = ["Life-Steal-Deck", "Life-Steal-Deck","Mana-Potion-Deck","Mana-Potion-Deck","Mana-Potion-Deck","Voodoo-Doll-Deck", "Voodoo-Doll-Deck", "Disarm-Deck", "Disarm-Deck", "Spell-Tome-Deck", "Smoke-Bomb-Deck", "Smoke-Bomb-Deck", "Arcane-Burst-Deck", "Health-Potion-Deck", "Health-Potion-Deck", "Bad-Medicine-Deck", "Bad-Medicine-Deck", "Magical-Bolt-Deck", "Magical-Bolt-Deck", "Magical-Bolt-Deck"]
+    
+    ///Function that is called once Player 1 presses the Select Class button to confirm their class.
+    ///This sets which deck Player 1 will use.
+    /// - Sources:
+    ///     - function adapted from https://stackoverflow.com/questions/31587181/sending-array-data-from-one-view-controller-to-another
+    /// - Parameters:
+    ///   - sender: function called when button is pressed
+    @IBAction func confirmSelection(_ sender: Any) {
+        switch segmentSelect.selectedSegmentIndex
+        {
+        case 0:
+            let defaults = UserDefaults.standard
+            //currDeck = warriorDeck.shuffled()
+            currDeck = warriorDeck
+            defaults.set(currDeck, forKey: "deck1")
+            
+        case 1:
+            let defaults = UserDefaults.standard
+            //currDeck = mageDeck.shuffled()
+            currDeck = mageDeck
+            defaults.set(currDeck, forKey: "deck1")
+        default:
+            currDeck = ["ERROR"]
+            break
+        }
+    }
+}
+
+/// Class that handles player 2's class/deck selection
+class SelectionDeck2: UIViewController {
+    var currDeck = ["Initial"];
+    override func viewDidLoad() {
+        //Nothing yet :)
+    }
+    @IBOutlet weak var segmentSelect: UISegmentedControl!
+    
+    let warriorDeck = ["Throwing-Knife-Deck", "Throwing-Knife-Deck","Liquid-Courage-Deck","Liquid-Courage-Deck","Liquid-Courage-Deck","Brass-Knuckles-Deck", "Brass-Knuckles-Deck", "Disarm-Deck", "Disarm-Deck", "Blacksmith-Deck", "Smoke-Bomb-Deck", "Smoke-Bomb-Deck", "Double-Edge-Deck", "Health-Potion-Deck", "Health-Potion-Deck", "Bad-Medicine-Deck", "Bad-Medicine-Deck", "Sword-Strike-Deck", "Sword-Strike-Deck", "Sword-Strike-Deck"]
+    
+    let mageDeck = ["Life-Steal-Deck", "Life-Steal-Deck","Mana-Potion-Deck","Mana-Potion-Deck","Mana-Potion-Deck","Voodoo-Doll-Deck", "Voodoo-Doll-Deck", "Disarm-Deck", "Disarm-Deck", "Spell-Tome-Deck", "Smoke-Bomb-Deck", "Smoke-Bomb-Deck", "Arcane-Burst-Deck", "Health-Potion-Deck", "Health-Potion-Deck", "Bad-Medicine-Deck", "Bad-Medicine-Deck", "Magical-Bolt-Deck", "Magical-Bolt-Deck", "Magical-Bolt-Deck"]
+    
+    ///Function that is called once Player 2 presses the Select Class button to confirm their class.
+    ///This sets which deck Player 2 will use.
+    /// - Sources:
+    ///     - function adapted from https://stackoverflow.com/questions/31587181/sending-array-data-from-one-view-controller-to-another
+    /// - Parameters:
+    ///   - sender: function called when button is pressed
+    @IBAction func confirmSelection(_ sender: Any) {
+        switch segmentSelect.selectedSegmentIndex
+        {
+        case 0:
+            let defaults = UserDefaults.standard
+            //currDeck = warriorDeck.shuffled()
+            currDeck = warriorDeck
+            defaults.set(currDeck, forKey: "deck2")
+        case 1:
+            let defaults = UserDefaults.standard
+            //currDeck = mageDeck.shuffled()
+            currDeck = mageDeck
+            defaults.set(currDeck, forKey: "deck2")
+        default:
+            currDeck = ["ERROR"]
+            break
+        }
+    }
+}
+
